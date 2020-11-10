69ad8bac4eade7a455bb95e70ccc7276abefdd86
Jakob <jakob.salfeld@cern.ch>
2020-11-10 12:57:51 +0100

    add customiseFor2018Input

--- HLTrigger/Configuration/python/customizeHLTforCMSSW.py
+++ HLTrigger/Configuration/python/customizeHLTforCMSSW.py
@@ -237,6 +237,12 @@ def customizeHLTfor42497(process):
     return process
 
 
+def customiseFor2018Input(process):
+    """Customise the HLT to run on Run 2 data/MC"""
+    process = customisePixelGainForRun2Input(process)
+    process = synchronizeHCALHLTofflineRun3on2018data(process)
+
+
 # CMSSW version specific customizations
 def customizeHLTforCMSSW(process, menuType="GRun"):
 

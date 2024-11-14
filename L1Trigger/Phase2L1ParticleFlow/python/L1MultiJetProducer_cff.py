d4919a85cde2d830014ac83f389bf503d4fdd145
Christopher Brown <c.brown19@imperial.ac.uk>
2024-11-14 12:46:25 +0100

    Proper path for the emulator

--- L1Trigger/Phase2L1ParticleFlow/python/L1MultiJetProducer_cff.py
+++ L1Trigger/Phase2L1ParticleFlow/python/L1MultiJetProducer_cff.py
@@ -1,5 +1,5 @@
 import FWCore.ParameterSet.Config as cms
-
+import os
 from L1Trigger.Phase2L1ParticleFlow.l1pfJetMet_cff import L1TPFJetsExtendedTask
 
 from L1Trigger.Phase2L1ParticleFlow.L1MultiJetProducer_cfi import  L1MultiJetProducer
@@ -8,7 +8,7 @@ l1tMultiJetProducerPuppi = L1MultiJetProducer.clone(
     maxJets = 16,
     minPt = 10,
     vtx = ("l1tVertexFinderEmulator","L1VerticesEmulation"),
-    MultiJetPath = cms.string("/builds/ml_l1/TrainTagger/CMSSW_14_0_0_pre3/src/hls4ml-jettagger/MultiJetBaseline")
+    MultiJetPath = cms.string("os.environ['CMSSW_BASE']+"/src/hls4ml-jettagger/MultiJetBaseline")
 )
 
 
